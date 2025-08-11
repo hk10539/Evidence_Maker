d30235149df9cbe28d332e4e1b0e0bef4490f133
Harsh Kumar <Hk10539@gmail.com>
2025-08-11 22:41:25 +0530

    11-08-2025 22:41

---
+++ src/main/java/org/evidencemaker/Main.java
@@ -0,0 +1,11 @@
+package org.evidencemaker;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
